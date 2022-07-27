0f9389da83913591599f10c285d855ae931e1207
Warissara0132 <6206021620132@fitm.kmutnb.ac.th>
2022-07-27 19:44:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<?php include('h.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center>
+<div class="jumbotron text-center">
+    <img src="start.jpg">
+  <h1>ระบบจัดการร้าน FinalShop</h1>
+  <p>เข้าสู่การจัดการหลังร้านได้ที่นี่</p>
+  <td align="center"><a href='admin_login.php' class='btn btn-warning btn-xs'>สำหรับ Admin</a></td>
+  <td align="center"><a href='member_login.php' class='btn btn-warning btn-xs'>สำหรับผู้ใช้</a></td>
+</div>
+
+</center>
+</body>
+</html>
